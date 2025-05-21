bd37c4da64676957c1eba5e7456340f731151e57
Jonathan Seyfert <jseyfert3@gmail.com>
2025-05-20 23:23:00 -0500

    feat: Config loading, parsing alerts
    Added "loadFile" to config, if path to a saved alert file is there, will load that for testing instead of getting alerts from api.weather.gov. Will not save config when loading config.
    
    Added some parsing of alerts to console for testing. Currently parses out alerts as # > Type > SAME codes > Has geometry. Also prints "WARNING!" if the SAME code is 040131, also for testing detecting a specific SAME code, which we want eventually.

--- main.cpp
+++ main.cpp
@@ -22,26 +22,62 @@ You should have received a copy of the GNU General Public License along with hyb
 
 int main(void)
 {    
-    std::ifstream config_file("config.json");
-    nlohmann::json config = nlohmann::json::parse(config_file);
-    config_file.close();
-    std::string state = config["state"].template get<std::string>(); // TODO: if this state isn't valid, then we will get a json back with an error saying so. Need to handle that properly
+	std::ifstream configFile("config.json");
+	nlohmann::json config = nlohmann::json::parse(configFile);
+	configFile.close();
+	std::string state = config["state"].template get<std::string>(); // TODO: if this state isn't valid, then we will get a json back with an error saying so. Need to handle that properly
+	
+	//std::string alerts = "";
+	std::string loadFile = config["loadFile"].template get<std::string>(); // if we want to load a config file instead of poll api.weather.gov
+	nlohmann::json alerts;
+	if(loadFile != "")
+	{
+		std::ifstream alertFile(loadFile);
+		alerts = nlohmann::json::parse(alertFile);
+		alertFile.close();
+		std::cout << "loaded alerts from file " << loadFile << "\n";
+	}
+	else
+	{
+		alerts = nlohmann::json::parse(fetchAlerts(state)); //TODO: will those error parsing if doesn't fetch results
+		std::cout << "Fetched alerts for state of " << state << "\n";
+	}
+	
 
-    std::string alerts = fetchAlerts(state);
-    if(alerts.size() > 0)
-    {
-        auto now = std::chrono::system_clock::now(); // get current time and date
-        std::chrono::zoned_time localTime{"America/Chicago", now}; // if wrong timezone, will throw error. Consider try/catch https://en.cppreference.com/w/cpp/chrono/time_zone/to_local
-        std::string datetime = std::format("{0:%F_%T%z}_", localTime); // formate into a string https://en.cppreference.com/w/cpp/chrono/system_clock/formatter
-        
-        std::string filename = datetime + state + "_alerts.json";
-        std::ofstream file(filename);
-        file << alerts;;
-        nlohmann::json data = nlohmann::json::parse(alerts);
-        std::cout << "Number of alerts: ";
-        std::cout << data["features"].size() << std::endl;
-        file.close();
-    }
-
-    return 0;
+	auto now = std::chrono::system_clock::now(); // get current time and date
+	std::chrono::zoned_time localTime{"America/Chicago", now}; // if wrong timezone, will throw error. Consider try/catch https://en.cppreference.com/w/cpp/chrono/time_zone/to_local
+	std::string datetime = std::format("{0:%F_%T%z}_", localTime); // formate into a string https://en.cppreference.com/w/cpp/chrono/system_clock/formatter
+	
+	if(loadFile == "") // save a file, only if we are not loading alerts from existing config file
+	{
+		std::string filename = datetime + state + "_alerts.json";
+		std::ofstream file(filename);
+		file << std::setw(4) << alerts; // "pretty print" the nlohmann::json to file
+		file.close();
+	}
+	
+	std::cout << "Number of alerts: ";
+	std::cout << alerts["features"].size() << std::endl;
+	
+	for(long unsigned int i = 0; i < alerts["features"].size(); i++) 
+	{
+		std::cout << "Alert #" << i << ":\n";
+		std::cout << "    " << alerts["features"][i]["properties"]["event"] << "\n";
+		std::cout << "    SAME codes:\n";
+		for(long unsigned int j = 0; j < alerts["features"][i]["properties"]["geocode"]["SAME"].size(); j++)
+		{
+			std::cout << "        " << alerts["features"][i]["properties"]["geocode"]["SAME"][j].template get<std::string>();
+			if(alerts["features"][i]["properties"]["geocode"]["SAME"][j].template get<std::string>() == "040131")
+				std::cout << " WARNING!\n";
+			else
+				std::cout << "\n";
+		}
+		std::cout << "    Has geometry: ";
+		if(alerts["features"][i]["geometry"] != nullptr)
+			std::cout << "Yes" << "\n";
+		else
+			std::cout << "No" << "\n";
+		std::cout << "\n";
+	}
+	return 0;
 } 
